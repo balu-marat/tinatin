3be30041e804c0c51b5e7051f79b97c2bb591530
Marat <marat197107@gmail.com>
2022-04-04 01:18:56 +0600

    Добавил изменения в index.html , add_to_cart.js

--- js/add_to_cart.js
+++ js/add_to_cart.js
@@ -1,3 +1,9 @@
 
 var pTitlte = document.getElementsByClassName('p__title');
+var productImgOne = document.getElementsByTagName('img');
 console.log(pTitlte[0].innerHTML);
+var b = productImgOne;
+console.log(b[12]);
+var priceReal = document.querySelector('.price__real');
+var c = priceReal;
+console.log(c.innerHTML);
