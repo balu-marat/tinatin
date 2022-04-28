8616366b07324a4a6b997d12340ebfb0217928e9
Marat <marat197107@gmail.com>
2022-04-28 23:41:39 +0600

    Внес изменения в html, css, js файлы

--- js/add_to_cart.js
+++ js/add_to_cart.js
@@ -10,25 +10,39 @@ var myFunctionProduct = function() {
   var delPrice = this.parentNode.parentNode.parentNode.querySelectorAll('.price del');
 
   var create_li = document.createElement('li');
+  create_li.classList.add('bag__item');
   console.log();
-  // console.log(titles[0].innerText);
   console.log();
-  // console.log(prices[0].innerText);
   console.log();
-  // console.log(images[0].src);
   console.log();
-  create_li.innerHTML = '<a class="left">\
+
+  create_li.innerHTML = '<a href="#" class="left">\
       <img src="'+images[0].src+'" alt="img50" class="n-m--img">\
     </a>\
     <div class="right">\
       <p>'+titles[0].innerText+'</p>\
       <p><span>'+prices[0].innerText+'</span><del class="">'+delPrice[0].innerText+'</del></p>\
-      <a><i class="fa-solid fa-trash-can"></i></a>\
+      <div class="trash"><a href="#"><i class="fa-solid fa-trash-can"></i></a></div>\
     </div>';
   var block_cart = document.querySelector('.dropdownMenuCart ul');
   block_cart.appendChild(create_li);
   alert('Товар добавлен в корзину !');
+
+  //   Удаление товара
+
+  var click__trash = document.querySelectorAll('.trash');
+  for (var i = 0; i < click__trash.length; i++) {
+    click__trash[i].addEventListener('click', del_func, false);
+  }
+
+  function del_func(evt) {
+    evt.preventDefault();
+    // alert('hello');
+    this.parentNode.parentNode.remove();
+    // console.log(this.parentNode);
+  }
 };
 for (var i = 0; i < cardProduct.length; i++) {
   cardProduct[i].addEventListener('click', myFunctionProduct, false);
 }
+
