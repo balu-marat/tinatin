24fc7f0072dc8589ff5ff269c50ec1a4036767af
Marat <marat197107@gmail.com>
2022-04-30 09:22:55 +0600

    Добавил изменения в html, css, js файлы

--- js/add_to_cart.js
+++ js/add_to_cart.js
@@ -5,23 +5,22 @@ console.log(cardProduct);
 
 var myFunctionProduct = function() {
   var titles = this.parentNode.parentNode.parentNode.querySelectorAll('.p__title');
-  var prices = this.parentNode.parentNode.parentNode.querySelectorAll('.price span');
+  var prices = this.parentNode.parentNode.parentNode.querySelectorAll('.price .oprice');
   var images = this.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.img img');
   var delPrice = this.parentNode.parentNode.parentNode.querySelectorAll('.price del');
 
   var create_li = document.createElement('li');
   create_li.classList.add('bag__item');
-  console.log();
-  console.log();
-  console.log();
-  console.log();
+  
 
   create_li.innerHTML = '<a href="#" class="left">\
       <img src="'+images[0].src+'" alt="img50" class="n-m--img">\
     </a>\
     <div class="right">\
       <p>'+titles[0].innerText+'</p>\
-      <p><span>'+prices[0].innerText+'</span><del class="">'+delPrice[0].innerText+'</del></p>\
+      <span class="currrency">$</span>\
+      <span class="oprice">'+prices[0].innerText+'</span>\
+      <del class="">'+delPrice[0].innerText+'</del></p>\
       <div class="trash"><a href="#"><i class="fa-solid fa-trash-can"></i></a></div>\
     </div>';
   var block_cart = document.querySelector('.dropdownMenuCart ul');
@@ -37,12 +36,27 @@ var myFunctionProduct = function() {
 
   function del_func(evt) {
     evt.preventDefault();
-    // alert('hello');
     this.parentNode.parentNode.remove();
-    // console.log(this.parentNode);
+
   }
+  total_price();
+    //   Получение суммы кол-ва товаров
+
+    
 };
 for (var i = 0; i < cardProduct.length; i++) {
   cardProduct[i].addEventListener('click', myFunctionProduct, false);
+}
+
+function total_price() {
+  var count__price = document.querySelectorAll('.right .oprice');
+
+  var total_price = 0;
+  for (var i = 0; i < count__price.length; i++) {
+    total_price = total_price + (+count__price[i].innerHTML);
+  }
+  // console.log(total_price);
+  document.getElementsByClassName('total__cart')[0].innerHTML = `$ ${total_price}`;
+  document.querySelectorAll('.shopcard__total .shop_t')[0].innerHTML = `$ ${total_price}`;
 }
 
